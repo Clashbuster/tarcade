064dcd90804b35160a01d8475c877838e31081e9
Jon Keam <jpkeam@gmail.com>
2021-06-26 15:23:19 -0400

    add api moves

--- src/Connect4/Connect4.js
+++ src/Connect4/Connect4.js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Connect4Column from  '../Connect4/Connect4Column.js'
 import Connect4Slot from  '../Connect4/Connect4Slot.js'
+import axios from 'axios';
+
+const HEIGHT = 4;
+const WIDTH = 4;
+
+const getPlayer2Move = (moves) => (
+  new Promise((resolve, reject) => (
+    axios.get('https://w0ayb2ph1k.execute-api.us-west-2.amazonaws.com/production', {
+      params: {
+        moves: JSON.stringify(moves)
+      }
+    }).then(({ data }) => resolve(data))
+      .catch(e => reject(e))
+  ))
+);
 
 const clone = (board) => {
   const newBoard = [];
@@ -11,9 +26,10 @@ const clone = (board) => {
   return newBoard;
 };
 
-const applyNewCoordinatesToBoardModel = (boardModel, turn, coordinates) => {
+const applyNewCoordinatesToBoardModel = (boardModel, turn, apiMoves, coordinates) => {
   const landingcoordinates = []
   const newBoardModel = clone(boardModel);
+  const newApiMoves = [...apiMoves, coordinates[0]];
   for(let y = 0; y < newBoardModel[coordinates[0]].length; y++){
     if(!newBoardModel[coordinates[0]][y]){
         newBoardModel[coordinates[0]][y] = turn? 1: 2;
@@ -22,10 +38,10 @@ const applyNewCoordinatesToBoardModel = (boardModel, turn, coordinates) => {
         break;
     }
   }
-  return {boardModel: newBoardModel, coordinates: landingcoordinates};
+  return {boardModel: newBoardModel, coordinates: landingcoordinates, apiMoves: newApiMoves};
 };
 
-const applyNewCoordinatesToBoard = (boardModel, turn, cb) => {
+const applyNewCoordinatesToBoard = (boardModel, turn, apiMoves, cb) => {
   const board = [];
   for(let x = 0; x < boardModel.length; x++){
     const newColumn = [];
@@ -34,7 +50,7 @@ const applyNewCoordinatesToBoard = (boardModel, turn, cb) => {
       if(boardModel[x][y] === 1 || boardModel[x][y] === 2){
         id = boardModel[x][y];
       }
-      newColumn.push(<Connect4Slot key={y} identifier={id} turn={turn} boardModel={boardModel} onClick={cb} coordinate={[x,y]} />);
+      newColumn.push(<Connect4Slot key={y} identifier={id} turn={turn} boardModel={boardModel} apiMoves={apiMoves} onClick={cb} coordinate={[x,y]} />);
     }
     board.push(<Connect4Column key={x} column={newColumn} />);
   }
@@ -48,21 +64,21 @@ const checkForWinningBoard = (boardModel, landingcoordinates, turn) => (
     checknegativeDiagonal(boardModel, landingcoordinates, turn? 1: 2)
 );
 
-const generateBoard = (boardModel, turn, onClickBoard) => {
+const generateBoard = (boardModel, turn, apiMoves, onClickBoard) => {
    boardModel.splice(0, boardModel.length);
-   for(let x = 0; x < 7; x++){
+   for(let x = 0; x < WIDTH; x++){
      let newColumn = [];
-     for(let y = 0; y < 6; y++){
+     for(let y = 0; y < HEIGHT; y++){
        newColumn.push(0);
      }
      boardModel.push(newColumn);
    }
 
    const board = [];
-   for(let i = 0; i < 7; i++){
+   for(let i = 0; i < WIDTH; i++){
      let newColumn = []
-     for(let k = 0; k < 6; k++){
-        newColumn.push(<Connect4Slot key={k} identifier={0} onClick={onClickBoard} turn={turn} boardModel={boardModel} coordinate={[i,k]} />);
+     for(let k = 0; k < HEIGHT; k++){
+        newColumn.push(<Connect4Slot key={k} identifier={0} onClick={onClickBoard} turn={turn} boardModel={boardModel} apiMoves={apiMoves} coordinate={[i,k]} />);
      }
      board.push(<Connect4Column key={i} column={newColumn} />);
    }
@@ -74,9 +90,9 @@ const checkpositiveDiagonal = (boardModel, coordinates, identifier) => {
   let counter = 0
 
   while(true){
-      if(coordinates[0] + counter +1 ===7){
+      if(coordinates[0] + counter +1 === WIDTH){
           break;
-      } else if(coordinates[1] + counter +1 ===6) {
+      } else if(coordinates[1] + counter +1 === HEIGHT) {
           break;
       }
       counter += 1
@@ -111,7 +127,7 @@ const checknegativeDiagonal = (boardModel, coordinates, identifier) => {
   let counter = 0
 
   while(true){
-      if(coordinates[0] + counter +1 ===7){
+      if(coordinates[0] + counter +1 === WIDTH){
           break;
       } else if(coordinates[1] - counter - 1 === - 1) {
           break;
@@ -134,7 +150,7 @@ const checknegativeDiagonal = (boardModel, coordinates, identifier) => {
 
       if(coordinates[0] + counter - (backwardsCounter +1) === -1){
           break;
-      } else if(coordinates[1] - counter + (backwardsCounter + 1) === 6){
+      } else if(coordinates[1] - counter + (backwardsCounter + 1) === HEIGHT){
           break;
       }
      
@@ -148,7 +164,7 @@ const checkhorizontal = (boardModel, coordinates, identifier) => {
   let counter = 0
 
   while(true){
-      if(coordinates[0] + counter + 1 === 7){
+      if(coordinates[0] + counter + 1 === WIDTH){
           break;
       } 
       counter += 1
@@ -181,7 +197,7 @@ const checkvertical = (boardModel, coordinates, identifier) => {
   let counter = 0
 
   while(true){
-      if(coordinates[1] + counter + 1 === 6){
+      if(coordinates[1] + counter + 1 === HEIGHT){
           break;
       } 
       counter += 1
@@ -210,33 +226,17 @@ const checkvertical = (boardModel, coordinates, identifier) => {
   return false
 };
 
-
-const updateRenderFromModel = (boardModel, cb) => {
-  let newBoardRender = [];
-  for(let x = 0; x < boardModel.length; x++){
-    let newColumn = [];
-    for(let y = 0; y < boardModel[x].length; y++){
-      let id = 0;
-      if(boardModel[x][y] === 1 || boardModel[x][y] === 2){
-        id = boardModel[x][y];
-      }
-      newColumn.push(<Connect4Slot identifier={id} onClick={cb} coordinate={[x,y]} />);
-    }
-    newBoardRender.push(<Connect4Column column={newColumn} />);
-  }
-  return newBoardRender;
-};
-
 const Connect4 = () => {
   const [board, setBoard] = useState(null);
   const [boardModel, setBoardModel] = useState([]);
   const [turn, setTurn] = useState(false);
   const [gamestarted, setGamestarted] = useState(false);
+  const [apiMoves, setApiMoves] = useState([]);
 
-  const onClickBoard = (coordinate, newTurn, newBoardModel) => {
+  const onClickBoard = (coordinate, newTurn, newBoardModel, newApiMoves) => {
     // figure out new board
     const updatedNewTurn = !newTurn;
-    const { boardModel: newBoardModel2, coordinates: newCoordinates } = applyNewCoordinatesToBoardModel(newBoardModel, updatedNewTurn, coordinate);
+    const { boardModel: newBoardModel2, coordinates: newCoordinates, apiMoves: newApiMoves2 } = applyNewCoordinatesToBoardModel(newBoardModel, updatedNewTurn, newApiMoves, coordinate);
 
     // check for win, if win update and end
     const winning = checkForWinningBoard(newBoardModel2, newCoordinates, updatedNewTurn);
@@ -248,18 +248,43 @@ const Connect4 = () => {
     }
 
     // if not win, calculate new board and update
-    updateBoard(newBoardModel2, updatedNewTurn);
+    updateBoard(newBoardModel2, updatedNewTurn, newApiMoves2);
+    getPlayer2Move(newApiMoves2).then(moves => {
+      const move = moves.pop();
+      const column = newBoardModel2[move];
+      let y = 0;
+      for (let i; i < column.length; i++) {
+        const value = column[i];
+        if (value) {
+          y = i;
+          break;
+        }
+      }
+      const { boardModel: newBoardModel3, coordinates: newCoordinates3, apiMoves: newApiMoves3 } = applyNewCoordinatesToBoardModel(newBoardModel2, newTurn, newApiMoves2, [move, y]);
+      updateBoard(newBoardModel3, newTurn, newApiMoves3);
+
+      // check for winning
+      const botWinning = checkForWinningBoard(newBoardModel3, newCoordinates3, newTurn);
+      // if win update
+      if (botWinning) {
+        setBoard(<div className="connect4-winner-statement">{`${newTurn ? 'Red' : 'Blue'} won the game!`}</div>);
+        return;
+      }
+    }).catch((e) => {
+      console.error(e);
+    });
   };
 
-  const updateBoard = (newBoardModel, newTurn) => {
-    const newBoard = applyNewCoordinatesToBoard(newBoardModel, newTurn, onClickBoard);
+  const updateBoard = (newBoardModel, newTurn, newApiMoves) => {
+    const newBoard = applyNewCoordinatesToBoard(newBoardModel, newTurn, newApiMoves, onClickBoard);
     setBoardModel(newBoardModel);
     setBoard(newBoard);
     setTurn(newTurn);
+    setApiMoves(newApiMoves);
   };
   
   const startGame = () => {
-    const { boardModel: newBoardModel, board: newBoard } = generateBoard(boardModel, turn, onClickBoard);
+    const { boardModel: newBoardModel, board: newBoard } = generateBoard(boardModel, turn, apiMoves, onClickBoard);
     setBoardModel(newBoardModel);
     setBoard(newBoard);
     setGamestarted(true);
--- src/Connect4/Connect4Slot.js
+++ src/Connect4/Connect4Slot.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Connect4Slot = ({ identifier, onClick, coordinate, turn, boardModel }) => {
+const Connect4Slot = ({ identifier, onClick, coordinate, turn, boardModel, apiMoves }) => {
   if (identifier === 1 || identifier === 2) {
     const className = `connect4-player${identifier}`;
     return <div className={className}></div>;
   }
-  return <div onClick={() => onClick(coordinate, turn, boardModel)} className="connect4-slot"></div>;
+  return <div onClick={() => onClick(coordinate, turn, boardModel, apiMoves)} className="connect4-slot"></div>;
 };
 
 export default Connect4Slot;
